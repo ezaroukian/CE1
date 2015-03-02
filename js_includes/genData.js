69de80a0865240eba7aafb0c5ce7d209762f361d
ezaroukian <egz@jhu.edu>
2015-03-02 12:46:57 +0000

    Update genData.js

--- js_includes/genData.js
+++ js_includes/genData.js
@@ -87,10 +87,9 @@ function ibexItem(FErule,CErule){
     var quest = "Do the following rules CORRESPOND?<br><br>"+FErule.rule+"<br><br>"+CErule.rule+"<br>";
     var ans = ["Yes","No"];
     var corr = 1;
-    alert(FErule.itype.substring(2,8));
-    //if (FErule.itype.substring(2,8)==CErule.itype.substring(2,8){
-    //    corr = 0;
-    //}
+    if (FErule.itype.substring(2,8)==CErule.itype.substring(2,8)){
+        corr = 0;
+    }
     ////testing
     //alert(FErule.type+"-"+CErule.type);
     return [ [type, "Question", {"q": quest, "as": ans, "hasCorrect": corr } ] ];
